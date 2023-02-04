d2ad6be739d6ece596278de455020f05481a8027
ardrasunil <s.ardrasunil@gmail.com>
2023-02-04 11:47:48 +0530

    Push FunNumber

---
+++ JavaLearning/src/FunNumber.java
@@ -0,0 +1,15 @@
+
+public class FunNumber {
+	public static void main(String args[]) {
+		 int fun = 3;
+		 String sun = "hello";
+		 fun = fun * 17;
+		 fun = ++fun;
+		 System.out.println("Fun is "+ fun);
+		 ++fun;
+		 System.out.print(fun + sun);
+		 System.out.print("\t Ardra");
+	}
+
+
+}
