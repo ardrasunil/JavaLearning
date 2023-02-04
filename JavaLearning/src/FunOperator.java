63d9d314726e2c52dff625d9d57d06a0bd986a2e
ardrasunil <s.ardrasunil@gmail.com>
2023-02-04 12:40:07 +0530

    .

---
+++ JavaLearning/src/FunOperator.java
@@ -0,0 +1,24 @@
+
+public class FunOperator {
+	public static void main(String args[])
+	{
+		int x = 5;
+		x +=5;
+		System.out.println(x);
+		float y = x;
+		System.out.println(y/11);
+		
+		System.out.println(x&3);
+		
+		//adding num and string
+		String a= "10";
+		int b = 20;
+		String c = a + b;
+		System.out.println(c);
+		
+		
+		
+		
+	}
+
+}
