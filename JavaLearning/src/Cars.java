721c508fddefd03461fb38aa87c8b3f080baea2f
ardrasunil <s.ardrasunil@gmail.com>
2023-02-04 13:07:46 +0530

    .

---
+++ JavaLearning/src/Cars.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Cars {
+	public static void main(String[] args) {
+		String[] Cars = {"Venue","Xcent","WagonR"};
+		System.out.println(Arrays.toString(Cars));
+		
+		//foreach
+		for (String i : Cars) {
+			  System.out.println(i);
+			}
+		
+	}
+
+}
