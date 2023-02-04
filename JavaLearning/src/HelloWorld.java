fbdb37317897b2561ff36adfbc7984198222a032
ardrasunil <s.ardrasunil@gmail.com>
2023-02-04 11:45:09 +0530

    Commit

---
+++ JavaLearning/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		String name = "Ardra";
+		System.out.println("Hello " +name);
+		
+		String firstName = "Ardra ";
+		String lastName = "Sunil";
+		String fullName = firstName + lastName;
+		System.out.println("my name is " +fullName);
+	}
+
+
+
+}
