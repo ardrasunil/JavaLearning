995fe652e4a1b2c5f5bc9bdd0fb011cdfaa33723
ardrasunil <s.ardrasunil@gmail.com>
2023-02-04 11:52:07 +0530

    push var2

---
+++ JavaLearning/src/Variable2.java
@@ -0,0 +1,24 @@
+
+public class Variable2 {
+	public static void main(String[] args){
+		int a=1000;
+		float f=a;
+		System.out.println(a);
+		System.out.println(f);
+		
+		float b = 20.6f;
+		int k = (int)b; //typecasting
+		System.out.println(b);
+		System.out.println(k);
+		
+		byte x=50;
+		byte y=10;
+		int l = x+y;
+		System.out.println(l);
+		double d = 456.988888888;
+		System.out.println(d);
+		
+		}
+
+
+}
