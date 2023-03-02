98bad953ec07c698041110c05c96a0dfbb327658
ardrasunil <85181779+ardrasunil@users.noreply.github.com>
2023-03-02 10:18:04 +0530

    .

--- StringLearning/src/TestString4.java
+++ StringLearning/src/TestString4.java
@@ -53,7 +53,7 @@ public static void main(String[] args)
 		}
 		
 		//System.out.println("Do you want to browse more(true/false)");
-		//isContinue = sc.nextBoolean();
+		isContinue = sc.nextBoolean();
 		}
 
 
