1f55095874f02308bbc5c8e83dac2ac6eb2d1b26
ardrasunil <85181779+ardrasunil@users.noreply.github.com>
2023-03-02 10:20:30 +0530

    updating

--- JavaExcersise4/src/Browser.java
+++ JavaExcersise4/src/Browser.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
 
 public class Browser {
 
+	 static String[] visits;
+	 
+	 void setVisits(StringBuffer url) {
+		 
+		 
+	 }
+	 public static void main(String[] args) {
+		 
+		 Scanner sc = new Scanner(System.in);
+		 
+		 Browser b = new Browser();
+		 StringBuffer url = new StringBuffer();
+		 url.append(sc.next());
+	 }
+
+		
 }
+
