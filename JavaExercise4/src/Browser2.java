3ebf6f8e7b72edf9f2bf37e0d9e9fcaf29867419
ardrasunil <85181779+ardrasunil@users.noreply.github.com>
2023-03-03 12:16:47 +0530

    Trying with StringBuilder

--- JavaExercise4/src/Browser2.java
+++ JavaExercise4/src/Browser2.java
@@ -1,51 +1,23 @@
 //trying with StringBuilder
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Browser2 {
-	StringBuilder[] visits;
 	
+	ArrayList<StringBuilder> visits = new ArrayList<>();
+			
 	void setVisits(StringBuilder url) {
 		
+		boolean found = false;
+		int index = 0;
 		
-		 boolean found = false;
-		 int index = 0;
-		 StringBuilder foundUrl = new StringBuilder();
-		 
-		 String[] splitString1 ;
-		 for(StringBuilder str : this.visits)
-				{ 	index += 1;
-				 	splitString1 = (str.toString()).split(" ## ");
-				 	if(splitString1[0].equals(url)) {
-				 		found = true;
-				 		foundUrl = str;
-				 		break;
-				 	}
-				}
-			 
-	 
-		 if(found) 
-		 {
-			 splitString1 = (foundUrl.toString()).split(" ## ");
-			 int presentCount = Integer.parseInt(splitString1[1]);
-			 visits[index-1].delete(0,visits[index-1].length());
-			 visits[index-1].append(splitString1[0]+" ## "+String.valueOf(presentCount+1));
-		 }
-		 else
-		 {
-			 System.out.println(visits.length);
-			 visits[visits.length].append(url);
-		 }
-		 
-		 foundUrl.delete(0, foundUrl.length());
+		
+	
 		
 	}
 	
 	void dispVisits() {
-		System.out.println("\t Pages visited with count");
-		 for(StringBuilder str:visits) {
-			 System.out.println(str);
-		 }
 		
 	}
 	
