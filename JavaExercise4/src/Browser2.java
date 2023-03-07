4e1f9f4f854aec00329e77cf2c435ec162c7a58c
ardrasunil <85181779+ardrasunil@users.noreply.github.com>
2023-03-07 23:58:07 +0530

    exercise 4 with StringBuilder modified

--- JavaExercise4/src/Browser2.java
+++ JavaExercise4/src/Browser2.java
@@ -1,47 +1,54 @@
 //trying with StringBuilder
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Browser2 {
-	
-	ArrayList<StringBuilder> visits = new ArrayList<>();
-			
-	void setVisits(StringBuilder url) {
-		
-		boolean found = false;
-		int index = 0;
-		
-		
-	
-		
-	}
-	
-	void dispVisits() {
-		
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		Browser2 b = new Browser2();
-		StringBuilder url = new StringBuilder();
-		 while(true) {
-			 
-			 System.out.println("enter the url you visited or enter exit");
-			 url.append(sc.next());
-			 System.out.println(url);
-			 if(url.equals("exit")) {
-				 break;
-			 }
-			 
-			 b.setVisits(url);
-			 //clear the buffer everytime
-			 url.delete(0, url.length());
-			 
-			 
-			 
-		 } 
+    StringBuilder visitedPages;
+    
+    public Browser2() {
+        visitedPages = new StringBuilder();
+    }
+    
+    public void visitPage(String url) {
+        int count = 1;
+        int firstIndex = visitedPages.lastIndexOf(url);
+        String[] splitString ;
+        
+        if(firstIndex>=0)
+        {
+        	
+        	int endIndex = visitedPages.indexOf("\n",firstIndex);
+        	String oldUrl = visitedPages.substring(firstIndex, endIndex);
+        	splitString = oldUrl.split(" ## ");
+        	count = Integer.parseInt(splitString[1]) + 1;
+        	String newUrl = splitString[0]+" ## "+count;
+        	visitedPages.replace(firstIndex, endIndex,newUrl );
+        }
+        else 
+        {
+        	visitedPages.append(url).append(" ## ").append(count).append("\n"); 
+        }
+        
+    }
+    
+    
+    
+    public String getHistory() {
+        return visitedPages.toString();
+    }
+    
+    public static void main(String[] args) {
+        Browser2 b = new Browser2();
+        b.visitPage("www.fb.com");
+        System.out.println(b.getHistory());
+        b.visitPage("www.google.com");
+        System.out.println(b.getHistory());
+        b.visitPage("www.fb.com");
+        System.out.println(b.getHistory());
+        b.visitPage("www.fb.com");
+        System.out.println(b.getHistory());
+        for(int i = 0;i<32;i++) {
+        b.visitPage("www.google.com");
+        }
+        b.visitPage("www.fb.com");
+        System.out.println(b.getHistory());
     }
 }
