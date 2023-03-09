9529e2d19731396c92ead50bf1ad9db18f12d901
ardrasunil <85181779+ardrasunil@users.noreply.github.com>
2023-03-09 22:03:55 +0530

    exception scenarios example

---
+++ ExceptionHandling/src/Example1.java
@@ -0,0 +1,52 @@
+
+public class Example1 {
+
+		public static void main(String[] args) {
+			
+			//arithmeticException
+			try {
+				int number = 100/0;
+			}
+			catch(ArithmeticException e)
+			{
+				System.out.println(e);
+			}
+		
+		
+			System.out.println("Exception handled and continued");
+			
+			String s = null;
+			//null pointer exception
+			try {
+				System.out.println(s.length());
+			}
+			catch(NullPointerException e) {
+				System.out.println(e);
+			}
+			
+			System.out.println("Exception handled and continued");
+			
+			//NumberFormat Exception
+			String str = "abc";
+			try {
+				int i = Integer.parseInt(str);
+			}
+			catch(NumberFormatException e) {
+				System.out.println(e);
+			}
+			
+			System.out.println("Exception handled and continued");
+			
+			//ArrayIndexOutOfBound
+			int[] a = new int[6];
+			try {
+				a[12] = 40;
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println(e);
+			}
+			
+			
+			System.out.println("Exception handled and continued");
+		}
+}
